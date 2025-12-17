f7adf1e41310001cb5c9c4d3c851f703ec9fb2f6
Lucke0302 <Lucke0302@gmail.com>
2025-12-17 12:49:40 -0300

    Adicionando tratamento de erros de overload do Gemini

--- Source/chatModel.js
+++ Source/chatModel.js
@@ -172,7 +172,7 @@ class ChatModel {
 
     //Recebe a resposta do Gemini utilizando o prompt recebido
     async getAiResponse(from, sender, isGroup, command, prompt) {    
-        const model = this.genAI.getGenerativeModel({ model: "gemini-1.5-flash-8b" });
+        const model = this.genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
         // Tenta até 3 vezes se der erro 503
         for (let i = 0; i < 3; i++) {

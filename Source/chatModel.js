49ece3fd020a416774dda65cf635a53655392de4
Lucke0302 <Lucke0302@gmail.com>
2025-12-18 16:16:48 -0300

    Erro encontrado e consertado.

--- Source/chatModel.js
+++ Source/chatModel.js
@@ -301,7 +301,7 @@ class ChatModel {
             limit = num;
         }
 
-        const msgCount = await this.getMessageCount(sender);
+        const msgCount = await this.getMessageCount(from);
         if (msgCount < 5) {
             throw new Error("FEW_MESSAGES");
         }

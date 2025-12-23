09a600efbf3fd1d4b840d1d271957631f4447d8a
Lucke0302 <Lucke0302@gmail.com>
2025-12-23 11:12:35 -0300

    Arrumando o broadcast com a previsão de amanhã

--- Source/index.js
+++ Source/index.js
@@ -203,7 +203,8 @@ async function connectToWhatsApp() {
                 
                 try {
                     const weatherComplement = await weatherCommandHandler.getWeather(targetCity);
-                    await broadcastToAllGroups(sock, "Bom dia, grupo! 🦖 O Bostossauro acordou e escolheu a violência.\n" + weatherComplement);
+                    const weatherForecastComplement = await weatherCommandHandler.getNextDayForecast(targetCity);
+                    await broadcastToAllGroups(sock, "Bom dia, grupo! 🦖 O Bostossauro acordou e escolheu a violência.\n" + weatherComplement + "\n" +);
                 } catch (error) {
                     console.error("❌ Erro no envio do clima agendado:", error);
                 }
